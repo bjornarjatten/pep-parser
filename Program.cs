3d0d044fc7887c665ef7fab80cc1907fc1ffef7f
Bjørnar Haugstad Jåtten <bjja@itu.dk>
2021-12-07 21:20:31 +0100

    Initial commit
    Ran donet new console to create a standard C# console application template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pep_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
